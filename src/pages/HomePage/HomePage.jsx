fed6937d2ebc792cbfa5838e5ac09a1e25447042
Volodymyr-Tym <163361581+Volodymyr-Tym@users.noreply.github.com>
2025-02-06 13:44:33 +0000

    Change logo path

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <Container className={styles.home_container}>
       <img
         className={styles.logo}
-        src="../../../logo_128px.png"
+        src="/logo_128px.png"
         alt="Your Private Electronic Phone book logo"
       />
 
